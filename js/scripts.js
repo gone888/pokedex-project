34755c1da7f6c6bd560e595c419766dec798136a
gone888 <bryanboyle.mail@gmail.com>
2025-04-23 14:24:35 -0400

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // pokemonRepository that stores the pokemonList array, the getAll and add functions.
 let pokemonRepository = ( function(){
-  let pokemonList = [
+  let repository = [
     {
       name: 'Charmander', 
       height: 2, 
@@ -18,11 +18,32 @@ let pokemonRepository = ( function(){
     }
   ];
   
+  // function that logs the pokemon object to console
+  function showDetails(pokemon){
+    console.log(pokemon);
+  };
+
+  // function that creates a list element and button for each pokemon
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    pokemonList.appendChild(listItem);
+    listItem.appendChild(button);
+    button.innerText = pokemon.name;
+    button.classList.add("button");
+
+    // button that on click will invoke a function that invokes the showDetails function
+    button.addEventListener('click', function (){
+      showDetails(pokemon);});
+  };
+
+  // This just returns the repository
   function getAll(){
-    return pokemonList;
+    return repository;
   }
 
-  // This is a function that adds pokemon to pokemonList and checks that the parameter is an object and contain both 'name' & 'height' keys.
+  // This is a function that adds pokemon to the repository and checks that the parameter is an object and contain both 'name' & 'height' keys.
   function add(pokemon){if (typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon){
     pokemonList.push(pokemon);
   }}
@@ -30,15 +51,13 @@ let pokemonRepository = ( function(){
   // This Allows the getAll and add function to be accessable globally.
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
   }
 })();
 
-// This is a function that prints the pokemonList elements and checks if they exceed a certain height. If they do, a message will appear next to their information noting their large stature.
-pokemonRepository.getAll().forEach(function(list){
-  if (list.height > 2) {
-    sizeAlert = ` - Wow, that's big!`;
-  } else {
-    sizeAlert = ''; 
-  } document.write(`<p> ${list.name} (Height: ${list.height}) ${sizeAlert} </p>`);
+// function that invokes the addListItem function with the repository as its parameter
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
 });
